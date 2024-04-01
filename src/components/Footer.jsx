e16c9ba1dcca42de410fba2f0a4c8ca720a90df8
Dhruv Patel <dhruvj1205@gmail.com>
2024-04-01 16:18:26 -0700

    Changes in JSON and Button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,19 @@ const Footer = () => {
     <footer className="bg-black text-white p-4 relative">
       <div className="max-w-5xl mx-auto flex flex-col lg:flex-row lg:items-center justify-between">
         <p className="text-center lg:text-left mb-4 lg:mb-0">
-          © 2023 Sarvagna Kadiya
+          ❤️ Dhruv Patel
         </p>
         <div className="flex justify-center lg:justify-end">
           <a
-            href="https://www.buymeacoffee.com/sarvagna"
+            href="mailto:dhruvj1205@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-teal-500 hover:bg-teal-600 text-gray-800 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
           >
-            contribute for buying me a Honda Civic 🥹
+            📧 Connect with me
           </a>
         </div>
-      </div>
+      </div> 
       <div className="absolute top-0 left-0 right-0 h-0.5 bg-gray-800"></div>
     </footer>
   );
