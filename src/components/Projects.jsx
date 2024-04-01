36a7371929004dbdd6680436305b3c21b7c00228
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-04-01 14:44:00 +0530

    Popup added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import projectsData from "../Data/projectsData.json";
+import ProjectPopup from "./ProjectPopup";
 
 const Projects = () => {
-  const [showAllProjects, setShowAllProjects] = useState(false);
-  const [expandedDescription, setExpandedDescription] = useState({});
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [showAll, setShowAll] = useState(false);
 
-  useEffect(() => {
-    setShowAllProjects(false);
-    setExpandedDescription({});
-  }, []);
-
-  const handleToggleShowAll = () => {
-    setShowAllProjects(!showAllProjects);
+  const openPopup = (project) => {
+    setSelectedProject(project);
   };
 
-  const toggleExpandDescription = (projectId) => {
-    setExpandedDescription((prevState) => ({
-      ...prevState,
-      [projectId]: !prevState[projectId],
-    }));
+  const closePopup = () => {
+    setSelectedProject(null);
   };
 
-  const toggleReadLess = (projectId) => {
-    setExpandedDescription((prevState) => ({
-      ...prevState,
-      [projectId]: false,
-    }));
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
   };
 
   return (
@@ -38,88 +28,49 @@ const Projects = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectsData
-            .slice(0, showAllProjects ? projectsData.length : 3)
+            .slice(0, showAll ? projectsData.length : 3)
             .map((project) => (
-              <div
-                key={project.id}
-                className="bg-gray-800 rounded-lg overflow-hidden hover:shadow-xl hover:-translate-y-1 flex flex-col"
-                style={{
-                  minHeight: expandedDescription[project.id] ? "auto" : "360px",
-                  height: expandedDescription[project.id] ? "auto" : "360px",
-                  //   transition: "min-height 10s, height 10s",
-                }}
-              >
-                <div className="p-6 flex flex-col justify-between flex-grow">
-                  <div className="mb-4">
-                    <div className="text-sm text-gray-400 mb-2">
-                      {project.category}
-                    </div>
-                    <h2 className="text-lg font-semibold mb-2">
-                      {project.title}
-                    </h2>
-                    <div
-                      className="text-gray-300"
-                      style={{
-                        maxHeight: expandedDescription[project.id]
-                          ? "none"
-                          : "200px",
-                        overflow: "hidden",
-                      }}
-                    >
-                      {project.description}
-                    </div>
+              <div key={project.id} className="h-full">
+                <div
+                  className="bg-gray-800 p-4 rounded-lg shadow cursor-pointer overflow-hidden hover:bg-opacity-50 transition-opacity duration-300"
+                  style={{
+                    height: "360px",
+                  }}
+                  onClick={() => openPopup(project)}
+                >
+                  <h2 className="text-lg font-semibold mb-2">
+                    {project.title}
+                  </h2>
+                  <div className="text-sm text-gray-400 mb-2">
+                    {project.category}
                   </div>
-                  <div className="flex justify-between items-center">
-                    {!expandedDescription[project.id] && (
-                      <button
-                        className="text-blue-500 hover:underline"
-                        onClick={() => toggleExpandDescription(project.id)}
-                      >
-                        Read More
-                      </button>
-                    )}
-                    {expandedDescription[project.id] && (
-                      <button
-                        className="text-blue-500 hover:underline"
-                        onClick={() => toggleReadLess(project.id)}
-                      >
-                        Read Less
-                      </button>
-                    )}
-                    <a
-                      href={project.githubLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="bg-gray-900 text-center py-2 px-4 text-blue-500 hover:underline rounded-lg "
-                    >
-                      View Project
-                    </a>
-                  </div>
+                  <p className="text-gray-300 overflow-hidden">
+                    {project.description}
+                  </p>
                 </div>
               </div>
             ))}
         </div>
-
-        {projectsData.length > 3 && (
-          <div className="mt-8">
-            {showAllProjects ? (
-              <button
-                className="text-blue-500 hover:underline"
-                onClick={handleToggleShowAll}
-              >
-                Hide
-              </button>
-            ) : (
-              <button
-                className="text-blue-500 hover:underline"
-                onClick={handleToggleShowAll}
-              >
-                Show All
-              </button>
-            )}
-          </div>
+        {!showAll && (
+          <button
+            onClick={toggleShowAll}
+            className="block mx-auto mt-4 bg-gray-800 text-white py-2 px-4 rounded-lg text-center hover:bg-gray-700"
+          >
+            Show All
+          </button>
+        )}
+        {showAll && (
+          <button
+            onClick={toggleShowAll}
+            className="block mx-auto mt-4 bg-gray-800 text-white py-2 px-4 rounded-lg text-center hover:bg-gray-700"
+          >
+            Hide
+          </button>
         )}
       </div>
+      {selectedProject && (
+        <ProjectPopup project={selectedProject} onClose={closePopup} />
+      )}
     </div>
   );
 };
