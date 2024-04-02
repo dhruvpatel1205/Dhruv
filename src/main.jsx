d1158f079025f6f89976823e7767e6354d9d4f6e
Dhruv Patel <dhruvj1205@gmail.com>
2024-04-01 23:09:35 -0700

    Changes made to main & navbar

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 const config = createConfig(
   getDefaultConfig({
     // Required API Keys
-    alchemyId: "O5NYvtwLMNG0LjAXPQEk0YJT2l3UxTAY", // or infuraId
-    walletConnectProjectId: "4c1fb4009a66dfd6a5f0c2babec3254b",
+    //alchemyId: "O5NYvtwLMNG0LjAXPQEk0YJT2l3UxTAY", // or infuraId
+    //walletConnectProjectId: "4c1fb4009a66dfd6a5f0c2babec3254b",
 
     // Required
     appName: "Your App Name",
